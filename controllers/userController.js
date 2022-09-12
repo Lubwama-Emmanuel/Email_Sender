eb62c65a190b09f0ff57d896bed2e3498ab68ba5
Rex Munil <lubwamaemmanuel1@gmail.com>
2022-09-12 15:27:32 +0300

    Good to go

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,12 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const Email = require("../utils/email")
 
 exports.createUser = catchAsync(async (req, res, next) => {
   const user = await User.create(req.body);
-
+  const url = `${req.protocol}://localhost:8080/api/emailSender/user`;
+  await new Email(user, url).sendWelcome();
+  console.log(url);
   res.status(200).json({
     status: "success",
     data: {
